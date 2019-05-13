cbac7c77cc6d1b998633721f2ffe45c0f6d24b7b
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-05-13 20:00:51 +0300

    v0.03

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+$(function(){
+    var lang= $('.arr')
+    lang.on('click', function () {
+
+        // $('.ukr').css('display', 'block')
+        // $('.eng').css('display', 'block')
+        $('.lang').css('width','150px')
+        $('.arr').css('transform', 'rotate(135deg)')
+        $('.arr').css('margin-bottom', '2px')
+
+    })
+
+
+
+})
