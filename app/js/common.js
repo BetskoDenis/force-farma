d913f77a29b11bd5554ebad7fb389c25e600451b
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-05-14 20:31:15 +0300

    v.006

--- app/js/common.js
+++ app/js/common.js
@@ -2,17 +2,24 @@ $(function(){
     $(window).scroll(function () {
          var windowPosition = $(this).scrollTop();
             topMenuTopValues = {'max-height':'165px',
-                'background-color': 'rgba(255, 255, 255, .9)'},
+                'background-color': 'rgba(255, 255, 255, .9)',
+                'transition':'all .5s'},
             topMenuScrollValues = {'max-height':'60px',
              'transition':'all .5s',
                 'background-color': 'rgba(255, 255, 255, 1)'};
+            topLogoScroll = {'height':'50px'}
+            topLogoTop = {'height':'150px'}
 
-                if(windowPosition == 0) {
+            if(windowPosition == 0) {
 
              $(".header").css(topMenuTopValues);
+             $(".logo_top_img").css(topLogoTop);
+
         } else {
 
             $(".header").css(topMenuScrollValues);
+            $(".logo_top_img").css(topLogoScroll);
+
 
         }
     });
@@ -22,22 +29,17 @@ $(function(){
 
 
         $('.arr').on('click', function () {
+            $('#arrButton').toggleClass('open')
+            $('.lang_wrapper').toggleClass('activeWrapper')
             var lang = $('.arr'),
                 trans = $('.arr').css('transform'),
                 ukr = $('.ukr'),
                 rus = $('.rus'),
                 eng = $('.eng');
+
+
 if ($('.arr').hasClass('open')){
-    $('.rus').css('transform', 'translateX(0px)')
-    $('.rus').css('display', 'flex')
-    $('.ukr').css('transform', 'translateX(500px)')
-    $('.ukr').css('display', 'none')
-    $('.eng').css('transform', 'translateX(500px)')
-    $('.eng').css('display', 'none')
-    $('.arr').css('transform', 'rotate(45deg)')
-    $('.arr').css('margin-bottom', '7px')
-    $('#arrbuttom').removeClass('open')
-}else {
+    $('.lang_wrapper').css('width', '150px')
     $('.rus').css('transform', 'translateX(0px)')
     $('.rus').css('display', 'flex')
     $('.ukr').css('transform', 'translateX(0px)')
@@ -46,7 +48,18 @@ if ($('.arr').hasClass('open')){
     $('.eng').css('display', 'flex')
     $('.arr').css('transform', 'rotate(135deg)')
     $('.arr').css('margin-bottom', '2px')
-    $('.arr').addClass('open')
+
+}else {
+    $('.lang_wrapper').css('width', '50px')
+    $('.rus').css('transform', 'translateX(0px)')
+    $('.rus').css('display', 'flex')
+    $('.ukr').css('transform', 'translateX(500px)')
+    $('.ukr').css('display', 'none')
+    $('.eng').css('transform', 'translateX(500px)')
+    $('.eng').css('display', 'none')
+    $('.arr').css('transform', 'rotate(45deg)')
+    $('.arr').css('margin-bottom', '7px')
+
 }
         });
 
