4bbbb6d3087629a5d721981ab2de8685ce30a396
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-05-15 16:35:57 +0300

    v0.07

--- app/js/common.js
+++ app/js/common.js
@@ -7,8 +7,11 @@ $(function(){
             topMenuScrollValues = {'max-height':'60px',
              'transition':'all .5s',
                 'background-color': 'rgba(255, 255, 255, 1)'};
-            topLogoScroll = {'height':'50px'}
-            topLogoTop = {'height':'150px'}
+            topLogoScroll = {'height':'50px',
+                'transition':'all .5s'}
+            topLogoTop = {'height':'150px',
+                'transition':'all .5s'},
+             windowWidth = $(window).width()
 
             if(windowPosition == 0) {
 
@@ -25,82 +28,35 @@ $(function(){
     });
 
 
-
-
-
         $('.arr').on('click', function () {
             $('#arrButton').toggleClass('open')
             $('.lang_wrapper').toggleClass('activeWrapper')
-            var lang = $('.arr'),
-                trans = $('.arr').css('transform'),
-                ukr = $('.ukr'),
-                rus = $('.rus'),
-                eng = $('.eng');
 
+            if ($('.arr').hasClass('open')){
+                $('.lang_wrapper').css('width', '150px')
+                $('.rus').css('transform', 'translateX(0px)')
+                $('.rus').css('display', 'flex')
+                $('.ukr').css('transform', 'translateX(0px)')
+                $('.ukr').css('display', 'flex')
+                $('.eng').css('transform', 'translateX(0px)')
+                $('.eng').css('display', 'flex')
+                $('.arr').css('transform', 'rotate(135deg)')
+                $('.arr').css('margin-bottom', '2px')
 
-if ($('.arr').hasClass('open')){
-    $('.lang_wrapper').css('width', '150px')
-    $('.rus').css('transform', 'translateX(0px)')
-    $('.rus').css('display', 'flex')
-    $('.ukr').css('transform', 'translateX(0px)')
-    $('.ukr').css('display', 'flex')
-    $('.eng').css('transform', 'translateX(0px)')
-    $('.eng').css('display', 'flex')
-    $('.arr').css('transform', 'rotate(135deg)')
-    $('.arr').css('margin-bottom', '2px')
+            }else {
+                $('.lang_wrapper').css('width', '50px')
+                $('.rus').css('transform', 'translateX(0px)')
+                $('.rus').css('transition', 'all .5s')
 
-}else {
-    $('.lang_wrapper').css('width', '50px')
-    $('.rus').css('transform', 'translateX(0px)')
-    $('.rus').css('display', 'flex')
-    $('.ukr').css('transform', 'translateX(500px)')
-    $('.ukr').css('display', 'none')
-    $('.eng').css('transform', 'translateX(500px)')
-    $('.eng').css('display', 'none')
-    $('.arr').css('transform', 'rotate(45deg)')
-    $('.arr').css('margin-bottom', '7px')
+                $('.ukr').css('transform', 'translateX(500px)')
+                $('.ukr').css('transition', 'all .5s')
+                $('.eng').css('transform', 'translateX(500px)')
+                $('.eng').css('transition', 'all .7s')
+                $('.arr').css('transform', 'rotate(45deg)')
+                $('.arr').css('margin-bottom', '7px')
 
-}
-        });
-
-
-
-
-    $('.langclass').on('click', function one () {
-        if ($('.ukr').on('click')) {
-
-
-            $('.rus').css('transform', 'translateX(500px)')
-            $('.rus').css('display', 'none')
-            $('.eng').css('transform', 'translateX(500px)')
-            $('.eng').css('display', 'none')
-
-            $('.arr').css('transform', 'rotate(45deg)')
-            $('.arr').css('margin-bottom', '7px')
-        } else if ($('.rus').on('click')) {
-            $('.ukr').css('transform', 'translateX(500px)')
-            $('.ukr').css('display', 'none')
-            $('.eng').css('transform', 'translateX(500px)')
-            $('.eng').css('display', 'none')
-
-            $('.arr').css('transform', 'rotate(45deg)')
-            $('.arr').css('margin-bottom', '7px')
-        } else if ($('.eng').on('click')) {
-            $('.ukr').css('transform', 'translateX(500px)')
-            $('.ukr').css('display', 'none')
-            $('.rus').css('transform', 'translateX(500px)')
-            $('.rus').css('display', 'none')
-
-            $('.arr').css('transform', 'rotate(45deg)')
-            $('.arr').css('margin-bottom', '7px')
-        }
-
-    });
-
-
-
-
-
+            }
+                    });
 
         $('.slider_wrapper').owlCarousel({
         items: 1,
@@ -108,6 +64,24 @@ if ($('.arr').hasClass('open')){
         dots: true,
         autoplay: true
 
-    })
+    });
+        $('.burger_menu').on('click', function () {
+            var widthSpan = $('.burger_span_two').css('width');
+              if (widthSpan == '0px') {
+                $('.burger_span_two').css('width', '50px')
+                $('.burger_span_one').css('transform', '')
+                $('.burger_span_three').css('transform', '')
+                $('.top_menu_mini').css('transform', 'translateX(500px)')
+        }
+              else{
+                $('.burger_span_two').css('width', '0')
+                $('.burger_span_one').css('transform', 'rotate(45deg)')
+                $('.burger_span_three').css('transform', 'rotate(-45deg)')
+                $('.top_menu_mini').css('transform', 'translateX(0px)')
+
+            }
+        })
+
+
 
 })
